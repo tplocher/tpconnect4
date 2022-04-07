b1700444ee2ba6fa2213b8a87cb122c210428cec
tplocher <67992848+tplocher@users.noreply.github.com>
2022-04-07 21:07:46 +0800

    changed to tplocher and tpconnect4; 20220407 21:07

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import { createBoard, playMove } from "./connect4.js";
 
 function getWebSocketServer() {
-  if (window.location.host === "aaugustin.github.io") {
-    return "wss://websockets-tutorial.herokuapp.com/";
+  if (window.location.host === "tplocher.github.io") {
+    return "wss://tpconnect4.herokuapp.com/";
   } else if (window.location.host === "localhost:8000") {
     return "ws://localhost:8001/";
   } else {
