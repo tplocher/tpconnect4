fbf838de18a5ba2c69785f64486aa63c27df8ead
tplocher <67992848+tplocher@users.noreply.github.com>
2022-04-08 14:03:03 +0800

    added runtime-error-msg (1x); 20220408 14:02

--- app.py
+++ app.py
@@ -102,8 +102,7 @@ async def start(websocket):
     watch_key = secrets.token_urlsafe(12)
     WATCH[watch_key] = game, connected
 
-    await error(websocket, f"start: game {JOIN[join_key]}")
-    await error(websocket, f"start: join_key {join_key}")
+    raise RuntimeError(f"start: game {JOIN[join_key]}; key {join_key}")
 
     try:
         # Send the secret access tokens to the browser of the first player,
