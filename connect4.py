805a2935b94bb5abad4ba02fcde0f7dc6facde8d
tplocher <67992848+tplocher@users.noreply.github.com>
2022-04-09 10:26:46 +0800

    changed error msg

--- connect4.py
+++ connect4.py
@@ -49,7 +49,7 @@ class Connect4:
 
         """
         if player == self.last_player:
-            raise RuntimeError(f"Wait! It isn't your turn. self.start = {self.start}")
+            raise RuntimeError("Wait! It isn't your turn.")
 
         row = self.top[column]
         if row == 6:
