5454e1935578216c702be4201cb9effadbe244b9
tplocher <67992848+tplocher@users.noreply.github.com>
2022-04-14 22:18:27 +0800

    textContent

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ function receiveMoves(board, websocket) {
         // Create links for inviting the second player and spectators.
         document.querySelector(".join").href = "?join=" + event.join;
         document.querySelector(".watch").href = "?watch=" + event.watch;
-        document.getElementById("txt1").SetHTML("Have a nice day!");
+        document.getElementById("txt1").textContent = "Have a nice day!";
         break;
       case "play":
         // Update the UI with the move.
