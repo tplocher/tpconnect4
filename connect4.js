3a54094a60558537711d55cb7b556e70be75a8cf
tplocher <67992848+tplocher@users.noreply.github.com>
2022-04-18 23:10:48 +0800

    Update connect4.js

--- connect4.js
+++ connect4.js
@@ -40,8 +40,8 @@ function playMove(board, player, column, row, moves) {
   if (!cellElement.classList.replace("empty", player)) {
     throw new Error("Cell must be empty.");
   } else {
-    document.getElementById("txt1").textContent = `Last move ${moves} was in column ${column}`;
-    document.getElementById("txt1").background = `${player}`;
+    document.getElementById("txt1").textContent = `Last move ${moves} was in column ${column+1}`;
+    document.getElementById("txt1").background = `red`;
   }
 }
 
